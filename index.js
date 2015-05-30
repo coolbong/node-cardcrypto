569a407f467c9a8116f03ec6365cefe28b8bcdbe
educe <educe.android@gmail.com>
2015-05-31 02:32:14 +0900

    add exports

--- index.js
+++ index.js
@@ -8,14 +8,26 @@ var aes = require('./lib/aes');
 var seed = require('./lib/seed');
 
 
+
 //hash
 var hash = require('./lib/hash');
 //padding
 var padding = require('./lib/padding');
 
+//mac
+var mac = require('./lib/mac');
+
+
+//bitwise
+var bitwise = require('./lib/bitwise');
+
+
 module.exports = {
     des: des,
     aes: aes,
     seed: seed,
-    padding: padding
+    padding: padding,
+    hash: hash,
+    mac: mac,
+    xor: bitwise.xor
 };
