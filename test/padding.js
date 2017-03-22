6d4e74da6f672f62b60fafcd9789323ed80a493c
coolbong <coolbong.works@gmail.com>
2017-03-22 11:06:31 +0900

    pkcs 5 padding added

--- test/padding.js
+++ test/padding.js
@@ -13,8 +13,27 @@ exports.padding = {
         assert.equal(result.toString('hex').toUpperCase(), '4E6F77206973207468652074696D6520666F7220616C6C20');
     },
     'ISO9797Method2' : function() {
-        //var buffer = new Buffer('Now is the time for all ', 'ascii');
-        //var result = padding.ISO9797Method2(buffer, 8);
-        //assert.equal(result.toString('hex').toUpperCase(), '4E6F77206973207468652074696D6520666F7220616C6C20');
+        var input = 'CAFEBABECAFEBA'; // 7 byte length
+        var result = padding.ISO9797Method2(input);
+        var answer = 'CAFEBABECAFEBA80';
+        assert.equal(result, answer);
+    },
+    'pkcs5_padding 01' : function() {
+        var input = 'CAFEBABECAFEBA'; // 7 byte length
+        var result = padding.pkcs5_padding(input);
+        var answer = 'CAFEBABECAFEBA01';
+        assert.equal(result, answer);
+    },
+    'pkcs5_padding 02' : function() {
+        var input = 'CAFEBABECAFE'; // 6 byte length
+        var result = padding.pkcs5_padding(input);
+        var answer = 'CAFEBABECAFE0202';
+        assert.equal(result, answer);
+    },
+    'pkcs5_padding 08' : function() {
+        var input = 'CAFEBABECAFEBABE'; // 8 byte length
+        var result = padding.pkcs5_padding(input);
+        var answer = 'CAFEBABECAFEBABE0808080808080808';
+        assert.equal(result, answer);
     }
 };
